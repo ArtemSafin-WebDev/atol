c12d5639e3f9b3ce45c76848ad588635e1032a57
Artem Safin <artemsafin.webdev@gmail.com>
2022-03-17 22:14:30 +0300

    Правки

--- src/js/productDropdowns.js
+++ src/js/productDropdowns.js
@@ -4,13 +4,16 @@ export default function productDropdowns() {
     icons.forEach(icon => {
         icon.addEventListener('click', event => {
             event.preventDefault();
+            icons.forEach(otherIcon => {
+                if (otherIcon !== icon) otherIcon.parentElement.classList.remove('active');
+            })
             icon.parentElement.classList.toggle('active');
         });
     });
 
-    // document.addEventListener('click', event => {
-    //     if (!(event.target.matches('.solutions__modal-feature') || event.target.closest('.solutions__modal-feature'))) {
-    //         icons.forEach(icon => icon.parentElement.classList.remove('active'));
-    //     }
-    // })
+    document.addEventListener('click', event => {
+        if (!(event.target.matches('.solutions__modal-feature') || event.target.closest('.solutions__modal-feature'))) {
+            icons.forEach(icon => icon.parentElement.classList.remove('active'));
+        }
+    })
 }
